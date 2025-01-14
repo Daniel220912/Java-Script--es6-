14d88e840a981a367cc4f16b8b2d9498a28dcf15
Daniel Balaguer <danielbalaguer1973@gmail.com>
2025-01-14 13:20:57 +0100

    JSON

--- DOOM/09-proyecto-lista-compra/js/scrip.js
+++ DOOM/09-proyecto-lista-compra/js/scrip.js
@@ -1,59 +1,139 @@
-//Traer las referencias de los elementos del DOM
+//Traer las refeéncias de los elementos del DOM
 const itemForm = document.querySelector("#item-form");
 const itemInput = document.querySelector("#item-input");
 const itemList = document.querySelector("#item-list");
+const btnClear = document.querySelector("#clear");
+const itemFilter = document.querySelector("#filter");
 
 /**
- * 
- * @param {SubmitEvent} evt 
+ *
+ * @param {SubmitEvent} evt
  */
 function addItem(evt) {
-    evt.preventDefault(); //Previene el efecto submit del formulario
+  evt.preventDefault(); //Previene el evento submit del formulario
 
-    //Validar que el input no esté vacío
-    const newItem = itemInput.value.trim();
-    //Si el campoestá vacío avisamos al usuario y salimos de la función
-    if (newItem === "") {
-        alert("Debes escribir un producto");
-        return;
-    }
+  //Validar que el input no esté vacio
+  const newItem = itemInput.value.trim();
+  //Si el campo está vacio avisamos al usuario y salimos de la funcion
+  if (newItem === "") {
+    alert("Por favor añade un tecto");
+    return;
+  }
 
-    //Inserta el elemento en el DOM
-    const li = createNewItem (newItem);
-    itemList.appendChild(li);
+  //Inserta el elemento en el DOM
+  const li = createNewItem(newItem);
+  itemList.appendChild(li);
 
-    //Limpar el campo de texto	
-    itemInput.value = "";
-
-    /********* Funciones para la creacion de los elementos de la lista */
-    function createNewItem(textItem) {
-        const li = document.createElement("li"); //Crear un elemento li
-        const text = document.createTextNode(textItem); //Crear un nodo de texto
-        li.appendChild(text); //Agregar el nodo de texto al elemento li <li> text </li>
-      
-        const button = createButton("remove-item btn-link text-red"); //Crear un elemento button //<button> </button>
-        li.appendChild(button); //Agregar el boton al li                  
-        return li;
-      }
-      
-      function createButton(clases) {
-          const button = document.createElement("button"); //Crear un elemento button //<button> </button>
-          button.className = clases; //Agregar clases al boton //<button class= "remove-item btn-link text-red"> </button>
-          
-          const icon = createIcon("fa-solid fa-xmark");
-          button.appendChild(icon); 
-          return button;
-      }
-      
-      function createIcon(clases) {
-        const icon = document.createElement("i"); 
-        icon.className = clases;
-        return icon;
-      
-      }
+  //Refrescamos el UI
+  checkUI();
+  //Limpiar el campo de texto
+  itemInput.value = "";
 }
 
+/*********     Funciones para la creación de los elementos de la lista **********/
+function createNewItem(textItem) {
+  const li = document.createElement("li");
+  const text = document.createTextNode(textItem);
+  li.appendChild(text);
 
+  const button = createButton("remove-item btn-link text-red");
+  li.appendChild(button);
+  return li;
+}
+
+function createButton(clases) {
+  const button = document.createElement("button");
+  button.className = clases;
+
+  const icon = createIcon("fa-solid fa-xmark");
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(clases) {
+  const icon = document.createElement("i");
+  icon.className = clases;
+  return icon;
+}
+
+function removeItem(evt) {
+  if (evt.target.parentElement.classList.contains("remove-item")) {
+    if (confirm("Vas ha eliminar el item")) {
+      evt.target.parentElement.parentElement.remove();
+      checkUI();
+    }
+  }
+}
+
+function clearItems() {
+  // itemList.innerHTML = "";
+  if (confirm("Vas ha eliminar toda la lista")) {
+    while (itemList.firstChild) {
+      itemList.removeChild(itemList.firstChild);
+    }
+    checkUI();
+  }
+}
+
+function filterItems(evt) {
+  const text = evt.target.value.toLowerCase();
+  const items = itemList.querySelectorAll("li");
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.includes(text)) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    btnClear.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    btnClear.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+}
+
+/***************   Local Storage functions ****************/
+function addItemToLocalStorage(item) {
+  //Traer los datos del localStorage
+  const itemsFromLocalStorage = getItemsFromLocalStorage();
+  //añadir el nuevo item al array
+  itemsFromLocalStorage.push(item);
+  //convertir el array a texto y lo guardamos
+  localStorage.setItem("lista", JSON.stringify(itemsFromLocalStorage))
+}
+
+function getItemsFromLocalStorage() {
+  let itemsFromLocalStorage;
+  if (localStorage.getItem("lista") === null) {
+    itemsFromLocalStorage = [];
+  } else {
+    itemsFromLocalStorage = JSON.parse(localStorage.getItem("lista"));
+  }
+  //Quiero devolver un array, no texto ni null
+  return itemsFromLocalStorage;
+
+  /*
+  const items = localStorage.getItem("lista");
+  if (items === null) {
+    return [];
+  }
+  return JSON.parse(items);
+  */
+}
 
 //Event Listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+btnClear.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems);
+
+checkUI();
