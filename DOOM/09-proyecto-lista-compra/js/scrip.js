12cc42cf96049b954fe59680f8b591d6b4c839ac
Daniel Balaguer <danielbalaguer1973@gmail.com>
2025-01-15 14:50:03 +0100

    lista-compra-finalizado

--- DOOM/09-proyecto-lista-compra/js/scrip.js
+++ DOOM/09-proyecto-lista-compra/js/scrip.js
@@ -4,7 +4,20 @@ const itemInput = document.querySelector("#item-input");
 const itemList = document.querySelector("#item-list");
 const btnClear = document.querySelector("#clear");
 const itemFilter = document.querySelector("#filter");
+const formBtn = document.querySelector("#mi-boton");
+let isEditMode = false;
 
+/**
+ * @description Obtiene los datos del LStorage y refresca la lista
+ */
+function displayItems() {
+  const lista = getItemsFromLocalStorage();
+  lista.forEach((item) => {
+    const li = createNewItem(item);
+    itemList.appendChild(li);
+  });
+  checkUI();
+}
 /**
  *
  * @param {SubmitEvent} evt
@@ -19,15 +32,135 @@ function addItem(evt) {
     alert("Por favor añade un tecto");
     return;
   }
+  //Miramos si estamos en modo edit
+  if (isEditMode) {
+    //Modo edicion
+    const itemToEdit = document.querySelector(".edit-mode");
 
-  //Inserta el elemento en el DOM
-  const li = createNewItem(newItem);
-  itemList.appendChild(li);
+    if (itemToEdit.textContent !== newItem) {
+      //Actualizar de verdad en caso de que sea distinto
+      //al valor nuevo del input y no exista en localSorage
+      if (checkIfItemExists(newItem) !== -1) {
+        alert("El item ya está en la lista");
+        itemInput.value = "";
+        return;
+      }
+      const itemIndex = checkIfItemExists(setItemToEdit.textContent);
+      updateItemFromLocalStorage(newItem, itemIndex);
+      itemToEdit.textContent = newItem;
+      const button = createButton("remove-item btn-link text-red");
+      itemToEdit.appendChild(button);
+    }
+    itemToEdit.classList.remove("edit-mode");
+    isEditMode = false;
+  } else {
+    //Modo agregar nuevo
+    //   //si el elemeto existe no se añade
+    if (checkIfItemExists(newItem) !== -1) {
+      alert("El item ya está en la lista");
+      itemInput.value = "";
+      return;
+    }
+    //Inserta el elemento en el DOM
+    const li = createNewItem(newItem);
+    itemList.appendChild(li);
+
+    //Añadir el elemento al localStorage
+    addItemToLocalStorage(newItem);
+  }
 
   //Refrescamos el UI
   checkUI();
   //Limpiar el campo de texto
   itemInput.value = "";
+}
+
+function removeItem(item) {
+  if (confirm("Vas ha eliminar el item")) {
+    item.remove();
+    removeItemFromLocalStorage(item.textContent);
+    checkUI();
+  }
+}
+//Mirar si eliminamos o editamos
+function onclickItem(evt) {
+  if (evt.target.parentElement.classList.contains("remove-item")) {
+    removeItem(evt.target.parentElement.parentElement);
+  } else if (evt.target.tagName === "LI") {
+    console.log("Entramos en modo edicion!!");
+    setItemToEdit(evt.target); //passamos el elemnto li a editar
+  }
+}
+
+/**  Modo edición **/
+function setItemToEdit(item) {
+  isEditMode = true;
+  console.log(item);
+  itemInput.value = item.textContent;
+  item.classList.add("edit-mode");
+  formBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Actualizar Item';
+  formBtn.style.backgroundColor = "#228b22";
+
+  //Eliminar el event listener del itemList
+  itemList.removeEventListener("click", onclickItem);
+
+  itemList.querySelectorAll("li").forEach((item) => {
+    item.querySelector("i").style.color = "#ccc";
+  });
+}
+
+function clearItems() {
+  // itemList.innerHTML = "";
+  if (confirm("Vas ha eliminar toda la lista")) {
+    while (itemList.firstChild) {
+      itemList.removeChild(itemList.firstChild);
+    }
+    //limpiar el localStorage
+    localStorage.removeItem("lista");
+    checkUI();
+  }
+}
+
+function filterItems(evt) {
+  const text = evt.target.value.toLowerCase();
+  const items = itemList.querySelectorAll("li");
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.includes(text)) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+/**
+ * @description Habilita o desabilita los elementos graficos según si
+ * hay items en la lista o no
+ */
+function checkUI() {
+  const items = itemList.querySelectorAll("li");
+  if (items.length === 0) {
+    btnClear.style.display = "none";
+    itemFilter.style.display = "none";
+  } else {
+    btnClear.style.display = "block";
+    itemFilter.style.display = "block";
+  }
+
+  formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Añadir Item';
+  formBtn.style.backgroundColor = "#333";
+
+  //Eliminar el event listener del itemList
+  itemList.addEventListener("click", onclickItem);
+
+  itemList.querySelectorAll("li").forEach((item) => {
+    item.querySelector("i").style.color = "red";
+  });
+
+  isEditMode = false;
 }
 
 /*********     Funciones para la creación de los elementos de la lista **********/
@@ -56,61 +189,15 @@ function createIcon(clases) {
   return icon;
 }
 
-function removeItem(evt) {
-  if (evt.target.parentElement.classList.contains("remove-item")) {
-    if (confirm("Vas ha eliminar el item")) {
-      evt.target.parentElement.parentElement.remove();
-      checkUI();
-    }
-  }
-}
-
-function clearItems() {
-  // itemList.innerHTML = "";
-  if (confirm("Vas ha eliminar toda la lista")) {
-    while (itemList.firstChild) {
-      itemList.removeChild(itemList.firstChild);
-    }
-    checkUI();
-  }
-}
-
-function filterItems(evt) {
-  const text = evt.target.value.toLowerCase();
-  const items = itemList.querySelectorAll("li");
-
-  items.forEach((item) => {
-    const itemName = item.firstChild.textContent.toLowerCase();
-
-    if (itemName.includes(text)) {
-      item.style.display = "flex";
-    } else {
-      item.style.display = "none";
-    }
-  });
-}
-
-function checkUI() {
-  const items = itemList.querySelectorAll("li");
-  if (items.length === 0) {
-    btnClear.style.display = "none";
-    itemFilter.style.display = "none";
-  } else {
-    btnClear.style.display = "block";
-    itemFilter.style.display = "block";
-  }
-}
-
 /***************   Local Storage functions ****************/
 function addItemToLocalStorage(item) {
   //Traer los datos del localStorage
   const itemsFromLocalStorage = getItemsFromLocalStorage();
   //añadir el nuevo item al array
   itemsFromLocalStorage.push(item);
-  //convertir el array a texto y lo guardamos
-  localStorage.setItem("lista", JSON.stringify(itemsFromLocalStorage))
+  //Convertir el array a texto y lo guardamos
+  localStorage.setItem("lista", JSON.stringify(itemsFromLocalStorage));
 }
-
 function getItemsFromLocalStorage() {
   let itemsFromLocalStorage;
   if (localStorage.getItem("lista") === null) {
@@ -129,11 +216,35 @@ function getItemsFromLocalStorage() {
   return JSON.parse(items);
   */
 }
+function removeItemFromLocalStorage(item) {
+  //Traer los datos del localStorage
+  let itemsFromLocalStorage = getItemsFromLocalStorage();
+  //eliminar el elemento del array
+  itemsFromLocalStorage = itemsFromLocalStorage.filter((i) => i !== item);
+  //Convertir el array a texto y lo guardamos
+  localStorage.setItem("lista", JSON.stringify(itemsFromLocalStorage));
+}
+function updateItemFromLocalStorage(item, itemIndex) {
+  //Traer los datos del localStorage
+  const itemsFromLocalStorage = getItemsFromLocalStorage();
+  itemsFromLocalStorage.splice(itemIndex, 1, item); //TODO con map
+  localStorage.setItem("lista", JSON.stringify(itemsFromLocalStorage));
+}
+/**
+ *
+ * @param {String} item
+ * @returns el indice del item si lo ecuentra, sinó -1
+ */
+function checkIfItemExists(item) {
+  //Traer los datos del localStorage
+  const itemsFromLocalStorage = getItemsFromLocalStorage();
+  //Mira si existe
+  return itemsFromLocalStorage.indexOf(item);
+}
 
 //Event Listeners
 itemForm.addEventListener("submit", addItem);
-itemList.addEventListener("click", removeItem);
+itemList.addEventListener("click", onclickItem);
 btnClear.addEventListener("click", clearItems);
 itemFilter.addEventListener("input", filterItems);
-
-checkUI();
+document.addEventListener("DOMContentLoaded", displayItems);
